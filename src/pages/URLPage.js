23ed2c4ce03e7309b59e84b64748c932e73b8a75
Yanne Sidibe <47622533+lyonscrawl@users.noreply.github.com>
2023-05-07 22:06:58 -0400

    fin1

--- src/pages/URLPage.js
+++ src/pages/URLPage.js
@@ -47,19 +47,19 @@ const tableIcons = {
   ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
   ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 };
-const options = ["SBTI", "CDP", "Ecovadis", "B-Corp"];
-// const ENDPOINT = "http://localhost:8080";
-const ENDPOINT = "https://greenly-backend.onrender.com"
+const options = ["SBTI", "CDP", "Ecovadis", "B-Corp", "Test 5 of SBTI"];
+const ENDPOINT = "http://localhost:8080";
+// const ENDPOINT = "https://greenly-backend.onrender.com"
 const socket = openSocket(ENDPOINT, { transports: ['websocket'] });
 
 // ----------------------------------------------------------------------
 
 export default function UserPage() {
 
-  let toastView;
+  let toastView
 
-  const [colDefs, setColDefs] = useState([
-    { title: "URL", field: "URL", lookup: { "SBTI": options[0], "CDP": options[1], "Ecovadis": options[2], "B-Corp": options[3] } },
+  const colDefs = [
+    { title: "URL", field: "URL", lookup: { "SBTI": options[0], "CDP": options[1], "Ecovadis": options[2], "B-Corp": options[3], "Test 5 of SBTI": options[4] } },
     { title: "isNew", field: "isNew", lookup: { "yes": "yes" } },
     { title: "Entreprise", field: "Entreprise", filtering: false },
     { title: "Localisation", field: "Localisation" },
@@ -73,11 +73,16 @@ export default function UserPage() {
     { title: "Domaine Web", field: "Domaine Web", filtering: false },
     { title: "Email", field: "Email", filtering: false },
     { title: "Telephone", field: "Telephone", filtering: false },
-  ])
+  ]
+  const columns = colDefs.map((column) => {
+    return { ...column };
+  });
   const [selectedOption, setSelectedOption] = useState(options[0])
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
+  const [fileData, setFileData] = useState([])
   const [newLead, setNewLead] = useState(0)
   const [fileName, setFileName] = useState("Scrap / Load CSV file")
+  const [isFile, setIsFile] = useState({val: false})
   const inputRef = useRef(null)
   const [scrapVal, setScrapVal] = useState("Scrap all datas")
   const [scrapCompVal, setScrapCompVal] = useState("Scrap all companies")
@@ -116,14 +121,16 @@ export default function UserPage() {
   const handleClick = () => {
     // 👇️ open file input box on click of another element
     inputRef.current.click();
+    setIsFile({val: true})
   };
 
   const importExcel = (e) => {
     const file = e.target.files[0]
     setFileName(capitalizeWords(file.name.split(".csv")[0]))
+    setNewLead(0)
+    setIsFile({val: true})
 
     const reader = new FileReader()
-
     reader.onload = (event) => {
       //parse data
       const bstr = event.target.result
@@ -135,25 +142,12 @@ export default function UserPage() {
       const fileData = utils.sheet_to_json(workSheet, { header: 1 })
       // console.log(fileData)
       const headers = fileData[0]
-      setNewLead(fileData.reduce((total, arr) => {
-        return total + arr.filter(word => word === "yes").length;
-      }, 0))
-      const heads = headers.map(
-        (head) => {
-          if(head === "isNew"){
-            return ({ title: head, field: head, lookup: { "yes": "yes" } })
-          } else if (head === "Poste"){
-            return ({ title: head, field: head })
-          } else{
-            return ({ title: head, field: head, filtering: false })
-          }
-        }
-      )
-      setColDefs(heads)
-
       //removing header
       fileData.splice(0, 1)
-      setData(convertToJson(headers, fileData))
+      let tmp = convertToJson(headers, fileData)
+      tmp.forEach((item) => { item.isNew = ""})
+      setData(tmp)
+      setFileData(tmp)
     }
 
     if (file) {
@@ -165,66 +159,76 @@ export default function UserPage() {
       }
     } else {
       setData([])
-      // setColDefs([])
     }
   }
 
   // SCRAP
   const getScrap = () => {
     if(scrapVal === "Scrap all datas"){
+      // setData([])
+      // setNewLead(0)
       setScrapVal("Stop Scrap")
       socket.emit("get_scrap", selectedOption)
       toastView = toast.loading('Scraping new leads of ' + selectedOption + '...\nThis can take a while, about 3 hours !')
     } else {
       socket.emit("stop_scrap", selectedOption)
       setScrapVal("Scrap all datas")
+      setIsFile({val: false})
     }
   }
 
   const getCompScrap = () => {
     if(scrapCompVal === "Scrap all companies"){
+      // setData([])
+      // setNewLead(0)
       setScrapCompVal("Stop Scrap")
       socket.emit("get_comp_scrap", selectedOption)
       toastView = toast.loading('Scraping companies of ' + selectedOption + '...\nThis can take a while, about 1 hour !')
     } else {
       socket.emit("stop_scrap", selectedOption)
       setScrapCompVal("Scrap all companies")
+      setIsFile({val: false})
     }
   }
 
   // USE EFFECT
   useEffect(() => {
     socket.on("scrap_result", result => {
-      if(!fileName.includes("Scrap / Load CSV file")){
+      if(isFile.val === true){
+        // console.log("file")
         if(result.length !== 0){
-          let arr = []
-          for(let r in result){
-            let tmp = false
-            for(let d in data){
-              if(r["URL Profil Linkedin"] === d["URL Profil Linkedin"]){
-                tmp = true
-                break;
-              }
-            }
-            if(tmp === false){
-              arr.push(r)
-            }
-          }
-          const count = arr.filter(item => item.isNew === 'yes').length;
+          const uniqueValues = result.filter(arr22Item => {
+            return !data.some(arr11Item => {
+              return arr22Item.URL === arr11Item.URL &&
+                     arr22Item.Entreprise === arr11Item.Entreprise &&
+                     arr22Item["URL Linkedin"] === arr11Item["URL Linkedin"]
+                    //  arr22Item.Industrie === arr11Item.Industrie 
+                    //  arr22Item.isNew !== arr11Item.isNew;
+            })
+          })
+          const combinedArray = [...data, ...uniqueValues];
+          setData(combinedArray);
+          const count = combinedArray.filter(item => item.isNew === 'yes').length;
           setNewLead(count)
         }
       } else {
+        // console.log("hey")
         setData(result);
         const count = result.filter(item => item.isNew === 'yes').length;
         setNewLead(count)
       }
-    });
+    }, []);
+    
     socket.on("scrap_end", inc => {
+      setScrapCompVal("Scrap all companies")
+      setScrapVal("Scrap all datas")
+      setFileName("Scrap / Load CSV file")
+      setIsFile({val: false})
       toast.dismiss(toastView);
       toast.success('fetched data leads');
       toast.dismiss();
     });
-  }, [data, fileName, toastView]);
+  }, [data, fileName, toastView, fileData, scrapCompVal, scrapVal, isFile]);
 
   return (
     <>
@@ -283,7 +287,7 @@ export default function UserPage() {
           <MaterialTable 
             title={fileName}
             data={data} 
-            columns={colDefs}
+            columns={columns}
             icons={tableIcons}
             options={{
               exportButton: true,  
@@ -301,7 +305,6 @@ export default function UserPage() {
                 new Promise((resolve, reject) => {
                   setTimeout(() => {
                     setData([...data, newData]);
-                    
                     resolve();
                   }, 1000)
                 }),
@@ -312,7 +315,6 @@ export default function UserPage() {
                     const index = oldData.tableData.id;
                     dataUpdate[index] = newData;
                     setData([...dataUpdate]);
-      
                     resolve();
                   }, 1000)
                 }),
@@ -323,7 +325,6 @@ export default function UserPage() {
                     const index = oldData.tableData.id;
                     dataDelete.splice(index, 1);
                     setData([...dataDelete]);
-                    
                     resolve()
                   }, 1000)
                 }),
